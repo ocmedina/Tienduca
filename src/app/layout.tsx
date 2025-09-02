b657b4f59c4816e8c012df6e9d906eabfb0aeaaf
ocmedina <octaviomedina239@gmail.com>
2025-09-02 00:54:18 -0300

    feat: Update page title for improved branding and clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Tienduca - Tu tienda de emprendedores" />
       <link rel="icon" href="/12.png" />
-      <title>Tienduca</title>
+      <title>Tienduca - Tu tienda de emprendedores</title>
     </head>
       <body>{children}</body>
     </html>
